9a2721bf09b80d2cf9fd31dfa706b63e4648ebf3
devsarim <55910649+devsarim@users.noreply.github.com>
2022-10-28 15:22:35 +0500

    remove unused module requires

--- lib/Resources/Languages.lua
+++ lib/Resources/Languages.lua
@@ -1,7 +1,4 @@
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-
 local HttpGet = require(script.Parent.Parent.HttpUtility.HttpGet)
-local t = require(ReplicatedStorage.Packages.t)
 
 local Url = "https://api.quran.com/api/v4/resources/languages"
 
